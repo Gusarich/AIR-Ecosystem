600234e2e4cc1b62fc4f60cc4872eb18a47a8df6
Gusarich <danielsedovzzz@gmail.com>
2021-08-11 12:31:01 +0300

    Loading lp pools from git

---
+++ main.js
@@ -0,0 +1,30 @@
+fetch('https://raw.githubusercontent.com/Gusarich/AIR-Ecosystem/main/data.json')
+.then(r => r.json())
+.then(r => {
+    let table = document.getElementsByClassName('staking-list')[0]
+
+    for (let i = 0; i < r.length; i += 1) {
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        let td2 = document.createElement('td')
+        let td3 = document.createElement('td')
+        let td4 = document.createElement('td')
+
+        td1.className = 'claim'
+        td3.className = 'tablet'
+        td4.className = 'desktop'
+
+        td1.appendChild(document.createTextNode(r[i]['TOKEN_1'] + ' / ' + r[i]['TOKEN_2']))
+        td2.appendChild(document.createTextNode(r[i]['APY'] + '%'))
+
+        td3.innerHTML = '0 <span class="f16">' + r[i]['TOKEN_1'] + '-' + r[i]['TOKEN_2'] + '</span>'
+        td4.innerHTML = '0 <span class="f16">' + r[i]['TOKEN_2'] + '</span>'
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+        tr.appendChild(td4)
+
+        table.appendChild(tr)
+    }
+})
