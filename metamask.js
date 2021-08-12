2de17f764bb62396e6e760ad1d56ee57dbeb22f0
Gusarich <danielsedovzzz@gmail.com>
2021-08-12 11:43:21 +0300

    connect wallet button

--- metamask.js
+++ metamask.js
@@ -1,4 +1,15 @@
+async function getAccount () {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+    const account = accounts[0]
+    console.log(account)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    const ethereumButton = document.querySelector('.connect-wallet')
+    ethereumButton.addEventListener('click', () => {
+        getAccount()
+    })
+
     if (typeof window.ethereum == 'undefined') {
         alert("I can't find your BSC wallet :(\nOpen this website with Metamask or Trust wallet")
         return
@@ -7,4 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
     ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
         console.log(accounts)
     })
-}
+})
