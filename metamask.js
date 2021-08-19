b532e5b4b7e7a873b01a22b03d4f96fb42b10b35
Gusarich <danielsedovzzz@gmail.com>
2021-08-19 12:29:59 +0300

    Fetching claimable amount

--- metamask.js
+++ metamask.js
@@ -1,3 +1,5 @@
+var accountUpdaterId;
+
 async function getAccount () {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
     const account = accounts[0]
@@ -5,13 +7,45 @@ async function getAccount () {
     let wallet = document.querySelector('.wallet')
     wallet.innerText = 'Connected: ' + account.slice(0, 4) + '...' + account.slice(-4)
 
-    reloadAccount(account)
-    setInterval(() => {
+    setTimeout(() => {
         reloadAccount(account)
-    }, 2000)
+    }, 500)
+    clearInterval(accountUpdaterId)
+    accountUpdaterId = setInterval(() => {
+        console.log('reload')
+        reloadAccount(account)
+    }, 3000)
 }
 
+ethereum.on('accountsChanged', function (accounts) {
+    let account = accounts[0]
+    let wallet = document.querySelector('.wallet')
+    wallet.innerText = 'Connected: ' + account.slice(0, 4) + '...' + account.slice(-4)
+
+    reloadAccount(account)
+    clearInterval(accountUpdaterId)
+    accountUpdaterId = setInterval(() => {
+        reloadAccount(account)
+    }, 3000)
+})
+
 document.addEventListener('DOMContentLoaded', () => {
+    window.web3_ = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/')
+
+    const abi = [
+        {
+            'type': 'function',
+            'name': 'claimableOf',
+            'constant': true,
+            'payable': false,
+            'stateMutability': 'view',
+            'inputs': [{'name':'account','type':'address'}, {'name':'token','type':'address'}],
+            'outputs': [{'name':'','type':'uint256'}]
+        }
+    ]
+
+    window.web3Contract = new window.web3_.eth.Contract(abi, '0x2fDc0abC687f729380ce075548537cc447DF59C4')
+
     if (typeof window.ethereum == 'undefined') {
         alert("I can't find your BSC wallet :(\nOpen this website with Metamask or Trust wallet")
         return
--- staking.js
+++ staking.js
@@ -1,3 +1,33 @@
+function normalizeNumber (n) {
+    if (n < 0.000001) return 0
+    if (n < 1000000) return Number(n.toPrecision(6))
+    return Math.ceil(n)
+}
+
+async function claimableOf (account, token) {
+    return await window.web3Contract.methods.claimableOf(account, token).call()
+}
+
+async function reloadAccount (account) {
+    let table = document.getElementsByClassName('staking-list')[0]
+
+    for (let i = 0; i < window.tokens.length; i += 1) {
+        let yourStake = table.getElementsByTagName('td')[2]
+        let yourProfit = table.getElementsByTagName('td')[3]
+        fetch('https://api-testnet.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + window.tokens[i][1] + '&address=' + account + '&tag=latest')
+        .then(r => r.json())
+        .then(async r => {
+            if (r.status == '1') {
+                let balance = normalizeNumber(r.result / 10 ** 18)
+                yourStake.innerHTML = balance + ' <span class="f16">' + window.tokens[i][0] + '</span>'
+                let claimable = await claimableOf(account, window.tokens[i][1])
+                yourProfit.innerHTML = claimable + ' <span class="f16">' + window.tokens[i][0] + '</span>'
+                console.log(claimable)
+            }
+        })
+    }
+}
+
 fetch('https://raw.githubusercontent.com/Gusarich/AIR-Ecosystem/main/data.json')
 .then(r => r.json())
 .then(r => {
@@ -14,12 +44,12 @@ fetch('https://raw.githubusercontent.com/Gusarich/AIR-Ecosystem/main/data.json')
         let td3 = document.createElement('td')
         let td4 = document.createElement('td')
 
-        td1.className = 'claim'
+        td1.className = 'claim noselect'
         td3.className = 'tablet'
         td4.className = 'desktop'
 
         td1.appendChild(document.createTextNode(r[i][0]))
-        td2.appendChild(document.createTextNode(r[i][2] + '%'))
+        td2.appendChild(document.createTextNode(r[i][2] * 365 + '%'))
 
         td3.innerHTML = '0 <span class="f16">' + r[i][0] + '</span>'
         td4.innerHTML = '0 <span class="f16">AIR</span>'
